1f62eaf864f3817fda51ece2694c16562003fa16
Lyly <khlbolatovna@gmail.com>
2022-06-16 18:21:03 +0600

    changed pi

--- circle.go
+++ circle.go
@@ -6,11 +6,11 @@ type Circle struct {
 }
 
 func (c *Circle) CalcPerimeter() float64 {
-	pi := 3.14
+	pi := 3.1415926535
 	return 2 * pi * c.Radius
 }
 
 func (c *Circle) CalcArea() float64 {
-	pi := 3.14
+	pi := 3.1415926535
 	return pi * (c.Radius * c.Radius)
 }
