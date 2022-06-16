7fb8b7a2dd740b041ca035ccab2d94d210725b51
Lyly <khlbolatovna@gmail.com>
2022-06-16 16:45:08 +0600

    added implementation for funcs

--- box.go
+++ box.go
@@ -1,5 +1,9 @@
 package golang_united_school_homework
 
+import (
+	"errors"
+	"reflect"
+)
 
 // box contains list of shapes and able to perform operations on them
 type box struct {
@@ -17,45 +21,120 @@ func NewBox(shapesCapacity int) *box {
 // AddShape adds shape to the box
 // returns the error in case it goes out of the shapesCapacity range.
 func (b *box) AddShape(shape Shape) error {
-	panic("implement me")
+	var counter int
+	if counter < b.shapesCapacity {
+		b.shapes = append(b.shapes, shape)
+		counter++
+	} else {
+		return errors.New("It goes out of the shapesCapacity range")
+	}
+	return nil
 }
 
 // GetByIndex allows getting shape by index
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) GetByIndex(i int) (Shape, error) {
-	panic("implement me")
+	var shape Shape
 
+	for indx, value := range b.shapes {
+		if indx == i {
+			return value, nil
+		}
+	}
+	return shape, errors.New("Shape by index doesn't exist or index went out of the range")
 }
 
-// ExtractByIndex allows getting shape by index and removes this shape from the list.
-// whether shape by index doesn't exist or index went out of the range, then it returns an error
+//ExtractByIndex allows getting shape by index and removes this shape from the list.
+//whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ExtractByIndex(i int) (Shape, error) {
-	panic("implement me")
+	var err error
+	var exist bool
+	var shape Shape
+	// circle := &Circle{}
 
+	for indx, value := range b.shapes {
+		if indx == i {
+			copy(b.shapes[indx:], b.shapes[indx+1:])
+			b.shapes = b.shapes[:len(b.shapes)-1]
+			shape = value
+			exist = true
+			indx--
+		}
+	}
+
+	if !exist {
+		err = errors.New("Shape by index doesn't exist or index went out of the range")
+	} else {
+		err = nil
+	}
+	return shape, err
 }
 
 // ReplaceByIndex allows replacing shape by index and returns removed shape.
 // whether shape by index doesn't exist or index went out of the range, then it returns an error
 func (b *box) ReplaceByIndex(i int, shape Shape) (Shape, error) {
-	panic("implement me")
+	var err error
+	var deletedShape Shape
+	var exist bool
+
+	for indx, value := range b.shapes {
+		if indx == i {
+			deletedShape = value
+			value = shape
+			exist = true
+			return deletedShape, nil
+		}
+	}
+	if !exist {
+		err = errors.New("Shape by index doesn't exist or index went out of the range")
+	} else {
+		err = nil
+	}
+	return deletedShape, err
 
 }
 
 // SumPerimeter provides sum perimeter of all shapes in the list.
 func (b *box) SumPerimeter() float64 {
-	panic("implement me")
-
+	var sum float64
+	for _, value := range b.shapes {
+		per := value.CalcPerimeter()
+		sum += per
+	}
+	return sum
 }
 
 // SumArea provides sum area of all shapes in the list.
 func (b *box) SumArea() float64 {
-	panic("implement me")
-
+	var sum float64
+	for _, value := range b.shapes {
+		per := value.CalcArea()
+		sum += per
+	}
+	return sum
 }
 
 // RemoveAllCircles removes all circles in the list
 // whether circles are not exist in the list, then returns an error
 func (b *box) RemoveAllCircles() error {
-	panic("implement me")
+	var err error
+	var circle Circle
+	var exist bool
+	// circle := &Circle{}
 
+	for indx, _ := range b.shapes {
+		if reflect.TypeOf(b.shapes[indx]) == reflect.TypeOf(circle) {
+			copy(b.shapes[indx:], b.shapes[indx+1:])
+			b.shapes = b.shapes[:len(b.shapes)-1]
+			exist = true
+			indx--
+		}
+	}
+
+	if !exist {
+		err = errors.New("Circles are not exist in the list")
+	} else {
+		err = nil
+	}
+	return err
 }
--- circle.go
+++ circle.go
@@ -4,3 +4,13 @@ package golang_united_school_homework
 type Circle struct {
 	Radius float64
 }
+
+func (c *Circle) CalcPerimeter() float64 {
+	pi := 3.14
+	return 2 * pi * c.Radius
+}
+
+func (c *Circle) CalcArea() float64 {
+	pi := 3.14
+	return pi * (c.Radius * c.Radius)
+}
